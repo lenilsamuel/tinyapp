50483ea1c4cb129a94ac39b607bb64b866b7d923
lenilsamuel <lenilvarghese@gmail.com>
2021-11-15 17:44:39 -0700

    installed ejs as a dependency and set view engine to ejs

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -23,3 +25,4 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
