c06092d02bc68f4478a412222a48b51392a0e65f
lenilsamuel <lenilvarghese@gmail.com>
2021-11-22 17:26:36 -0700

    add POST route for /logout

--- express_server.js
+++ express_server.js
@@ -27,12 +27,13 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render('urls_index', templateVars);
 })
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {username: req.cookies["username"]};
+  res.render('urls_new', templateVars);
 })
 
 app.post("/urls", (req, res) => {
@@ -55,14 +56,18 @@ app.post('/urls/:id', (req, res) => {
   res.redirect('/urls/')
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
+
 app.post('/login', (req, res) => {
   res.cookie("username", req.body.username);
-  console.log(req.body.username);
   res.redirect('/urls');
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
